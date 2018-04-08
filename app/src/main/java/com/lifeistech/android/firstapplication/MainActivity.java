1d09ca50ebb94373312780e50c09e9f65a220b9d
haraguchiyui <yuimaruuu.06.10@gmail.com>
2018-04-08 16:56:26 +0900

    最初のアプリ

---
+++ app/src/main/java/com/lifeistech/android/firstapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.lifeistech.android.firstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void changeText(View v){
+        ((Button) v).setText("クリックされました！");
+    }
+
+
+}
